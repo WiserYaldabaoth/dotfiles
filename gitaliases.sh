ab9f3a8c38880f677809d42a356838af5420f105
Brian A Mejorado <bam9523@rit.edu>
2016-04-16 17:12:09 -0700

    Provide gitaliases.sh script to supply aliases.
    Since .gitconfig doesn't have any "source" command available (as far as
    I can tell), this script will make most of the same adjustments I'd
    need.

---
+++ gitaliases.sh
@@ -0,0 +1,15 @@
+# git alias -- Make a new alias
+git config --global alias.alias "!sh -c '[ $# = 2 ] && git config --global alias.\"$1\" \"$2\" && exit 0 || echo \"usage: git alias <new alias> <original command>\" >&2 && exit 1' -"
+
+# git fl -- Pretty git log
+git config --global alias.fl log "--oneline --graph --all --decorate=full"
+
+# git fld -- Pretty git log sorted by date
+git config --global alias.fl log "--oneline --graph --all --decorate=full --date-order"
+
+# git renameTag -- Rename an existing tag
+git config --global alias.renameTag "!sh -c 'set -e;git tag $2 $1; git tag -d $1;git push origin :refs/tags/$1;git push --tags' -"
+
+# git new -- See new commits (typically after git pull)
+git config --global alias.new "!sh -c 'git log $1@{1}..$1@{0} \"$@\"'"
+
